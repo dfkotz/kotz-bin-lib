eec661d1a2d5a84ec6a3f7243a5edda52077e657
David Kotz <David.F.Kotz@dartmouth.edu>
2021-12-28 12:46:23 -0500

    add Doe to the daily calendar

--- scripts/mac/daily-email.sh
+++ scripts/mac/daily-email.sh
@@ -9,11 +9,11 @@ PAM=pcjenkins@mac.com
 JOHN=john.lyme@mac.com
 MARA=mara.lyme@mac.com
 ANDY=andy.lyme@mac.com
-ISABEL=isabel_eich@hotmail.com
 DAD=johnkotz@mac.com
 MOM=katiekotz@icloud.com
 AMY=aejenkins1263@gmail.com
-KIDS="$JOHN $MARA $ANDY"
+ISABEL=isabel_eich@hotmail.com
+DOE=jenkd@musc.edu
 
 # COMICS
 # send me the daily comic strips (timed for morning delivery)
@@ -23,4 +23,4 @@ comic-fbfw               $DAVE
 
 # CALENDAR
 # send the Kotz daily calendar
-daily-calendar $DAVE $ANDY $PAM $MARA $JOHN $DAD $MOM $AMY $ISABEL
+daily-calendar $DAVE $ANDY $PAM $MARA $JOHN $DAD $MOM $AMY $ISABEL $DOE
