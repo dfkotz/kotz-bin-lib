edd7219fbce50c7dbd259c7dd0a3efdbdad976b2
David Kotz <David.F.Kotz@dartmouth.edu>
2022-01-12 06:34:25 -0500

    refine the checking of photos, and pull it out as its own script, to streamline daily-backup script

--- scripts/mac/daily-backup.sh
+++ scripts/mac/daily-backup.sh
@@ -1,37 +1,14 @@
 #!/bin/bash
 #
-# daily-backup - a script to run a daily backup of my laptop
+# daily-backup - a daily backup of my laptop
 #
 # Two aspects:
 #   Check the photo directories for integrity;
 #   Time-Machine backup to local hard drive.
-#
-# BUGS: photo-directory names must not have embedded spaces.
 
-dirs=( ~/Personal/Photos/Lightroom ~/Dropbox/Lightroom )
+daily-photos.sh
 
-# look for newly arrived subdirectories - which should maybe be checked
-
-for dir in "${dirs[@]}";
-do
-    (cd "$dir"; pwd; ls | diff .ls - ) || exit 1
-done
-
-log=/tmp/daily-backup$$.log
-
-# check photo collections for new files
-echo metacheck --expand...
-metacheck --expand "${dirs[@]}"  > "$log" \
-    || mail -s "metacheck-expand" $USER < "$log"
-echo hashcheck --update...
-hashcheck --update "${dirs[@]}" > "$log" \
-    || mail -s "hashcheck-update" $USER < "$log"
-# check photo collections for integrity
-echo metacheck --verify...
-metacheck --verify "${dirs[@]}" > "$log" \
-    || mail -s "metacheck-verify" $USER < "$log"
-
-# run the Time Machine backup, and inherit its exit status
+# run the Time Machine backup, and check its exit status
 if tm-backup.sh
 then
     terminal-notifier -title "daily-backup" -sound Hero -message "backup succeeded"
---
+++ scripts/mac/daily-photos.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# daily-photos - a daily checkup of my photos
+#
+# BUGS: photo-directory names must not have embedded spaces.
+
+dirs=( ~/Personal/Photos/Lightroom ~/Dropbox/Lightroom )
+
+log=/tmp/daily-photos$$.log
+
+# look for newly arrived subdirectories - which should maybe be checked
+for dir in "${dirs[@]}";
+do
+    (cd "$dir"; pwd; ls | diff .ls - ) > $log \
+    || mail -s "daily-photos detected changes to $dir" $USER < "$log"
+done
+
+# check photo collections for new files, and add them
+echo metacheck --expand...
+metacheck --expand "${dirs[@]}"  > "$log" \
+    || mail -s "metacheck-expand" $USER < "$log"
+echo hashcheck --update...
+hashcheck --update "${dirs[@]}" > "$log" \
+    || mail -s "hashcheck-update" $USER < "$log"
+
+# check photo collections for integrity
+echo metacheck --verify...
+metacheck --verify "${dirs[@]}" > "$log" \
+    || mail -s "metacheck-verify" $USER < "$log"
+
