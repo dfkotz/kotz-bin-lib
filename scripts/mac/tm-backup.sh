9da208cf6c912c51975812c4455ccc8c58270728
David Kotz <David.F.Kotz@dartmouth.edu>
2022-01-29 08:13:26 -0500

    restructured to wait 10 minutes for one of the backup drives to appear

--- scripts/mac/tm-backup.sh
+++ scripts/mac/tm-backup.sh
@@ -1,44 +1,49 @@
 #!/bin/bash
 #
-# tm-backup - a script to run a Time Machine backup of my laptop
-#
+# tm-backup - a script to run a Time Machine backup of my laptop.
+# Need to know the name of the backup drive and UUID of its backup;
+# find them with `tmutil destinationinfo`
 
-# run a backup and eject the drive
-function backup-then-eject() {
-    # expect two parameters:
-    # (find these with `tmutil destinationinfo`
-    backup="$1" # basename of the backup volume
-    UUID="$2"   # UUID of the TM on that volume
-    # verify backup volume is available
-    if [ -d "/Volumes/$backup" ]; then
-        # start Time Machine backup
-        echo "start Time Machine backup to $backup..."
-        if tmutil startbackup --block --destination "$UUID"; then
-            echo "SUCCESS"
-            eject "$backup"
-            return 0
-        else # Time Machine failed
-            echo "FAILED - Time Machine exited non-zero"
-            eject "$backup"
-            return 1
-        fi
-    else # backup disk not found
-        return 2
+# return 0 (true) if the named backup ($1) is mounted
+function is-mounted() {
+    name="$1"
+    echo "looking for $name"
+    if [ -d "/Volumes/$name/Backups.backupdb" ]; then
+        return 0
+    else
+        return 1
     fi
 }
 
-# backup to black SSD disk
-backup-then-eject Kotz-TM-SSD-black 596A9425-4CBD-48CF-A090-5B868B9983F2
-black=$?
+# Wait for one drive to be mounted; give up after ten minutes
+for try in {1..20};
+do
+    name=Kotz-TM-SSD-black
+    UUID=596A9425-4CBD-48CF-A090-5B868B9983F2
+    is-mounted "$name" && break
 
-# backup to blue SSD disk
-backup-then-eject Kotz-TM-SSD-blue FB2E7472-CF6D-430F-9510-75B51F233DDB
-blue=$?
+    name=Kotz-TM-SSD-blue
+    UUID=FB2E7472-CF6D-430F-9510-75B51F233DDB
+    is-mounted "$name" && break
 
-if (( $black == 0 || $blue == 0 )); then
-    echo at least one backup succeeded
+    name=
+    UUID=
+    sleep 30
+done
+
+if [ "$UUID" == "" ]; then
+    echo BACKUP NOT FOUND
+    exit 2
+fi
+
+# backup to that disk
+echo "backing up to $name..."
+if tmutil startbackup --block --destination "$UUID"; then
+    eject "$name"
+    echo backup succeeded
     exit 0
 else
+    eject "$name"
     echo BACKUP FAILED
-    exit 99
+    exit 1
 fi
