9d63f956d6110b0de8dc710ad3550d6508839ae1
David Kotz <David.F.Kotz@dartmouth.edu>
2022-08-16 17:54:30 -0400

    revised daily backup system, and red-drive backup system, using Google Drive as the master copy of older photos

--- scripts/mac/daily-photos.sh
+++ scripts/mac/daily-photos.sh
@@ -3,20 +3,35 @@
 # daily-photos - a daily checkup of my photos
 #
 
-dirs=( ~/Personal/Photos/Lightroom/*/ ~/Dropbox/Lightroom/*/ )
-
 log=/tmp/daily-photos$$.log
 
-# check photo collections for new files, and add them
-echo metacheck --expand...
-metacheck --expand "${dirs[@]}"  > "$log" \
-    || mail -s "metacheck-expand" $USER < "$log"
-echo hashcheck --update...
-hashcheck --update "${dirs[@]}" > "$log" \
-    || mail -s "hashcheck-update" $USER < "$log"
+# check all arguments (directories) for new and missing files
+function checkdirs()
+{
+    # check photo collections for new files, and add them
+    echo metacheck --expand...
+    time metacheck --expand "$@"  > "$log" \
+        || mail -s "metacheck-expand" $USER < "$log"
 
-# check photo collections for integrity
-echo metacheck --verify...
-metacheck --verify "${dirs[@]}" > "$log" \
-    || mail -s "metacheck-verify" $USER < "$log"
+    echo hashcheck --update...
+    time hashcheck --update "$@" > "$log" \
+        || mail -s "hashcheck-update" $USER < "$log"
 
+    # check photo collections for integrity
+    echo metacheck --verify...
+    time metacheck --verify "$@" > "$log" \
+        || mail -s "metacheck-verify" $USER < "$log"
+
+    echo hashcheck --random...
+    time hashcheck --random "$@" > "$log" \
+        || mail -s "hashcheck-random" $USER < "$log"
+}
+
+echo LAPTOP DIRECTORIES...
+cd ~/Personal/Photos/Lightroom/ && checkdirs *
+
+echo
+echo GOOGLE DIRECTORIES...
+cd ~/Lightroom/ && checkdirs *
+
+rm -f "$log"
